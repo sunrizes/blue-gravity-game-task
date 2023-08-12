9441f482428bb6ff8f2cc32e06c62f233a6e1af7
Milos Tatic <sunrizes99@gmail.com>
2023-08-12 15:01:14 +0200

    Main menu update

--- ClothesShop/Assets/Scripts/MainMenu/MainMenuManager.cs
+++ ClothesShop/Assets/Scripts/MainMenu/MainMenuManager.cs
@@ -1,18 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void OnClickStartButton()
     {
-        
+        SceneManager.LoadScene("02_MainGame");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnClickQuitButton()
     {
-        
+        Application.Quit();
     }
 }
